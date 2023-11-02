e7255ab70f839218b2a62e166f932717f32ae050
AitsuYuyu <136203413+AitsuYuyu@users.noreply.github.com>
2023-11-02 07:45:03 -0500

    feat: :sparkles: remplaze structure simple

--- 2_4EntidadesRelacionales/2_4Identificador_EntidadesRelacionales.sql
+++ 2_4EntidadesRelacionales/2_4Identificador_EntidadesRelacionales.sql
@@ -3,115 +3,127 @@ CREATE DATABASE 2_4Modelo1;
 USE 2_4Modelo1;
 
 CREATE TABLE Inscription(
-    inscription_id INT NOT NULL AUTO_INCREMENT,
+    inscription_id INT NOT NULL,
     document INT NOT NULL,
-    dateInscription DATE NOT NULL,
-    PRIMARY KEY (inscription_id)
+    dateInscription DATE NOT NULL
 );
 
 CREATE TABLE Register(
-    register_id INT NOT NULL AUTO_INCREMENT,
+    register_id INT NOT NULL,
     informationBasic_id INT NOT NULL,
-    faculty_id BIGINT NOT NULL,
-    course_id INT NOT NULL,
-    PRIMARY KEY (register_id),
-    FOREIGN KEY (informationBasic_id) REFERENCES InformationBasic(document),
-    FOREIGN KEY (faculty_id) REFERENCES Faculty(faculty_id),
-    FOREIGN KEY (course_id) REFERENCES Courses(course_id)
+    faculty_id INT NOT NULL,
+    course_id INT NOT NULL
 );
 
 CREATE TABLE AcademicSession(
-    academicSession_id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (academicSession_id)
+    academicSession_id INT NOT NULL
+    
 );
-
 CREATE TABLE Faculty(
-    faculty_id INT NOT NULL AUTO_INCREMENT,
+    faculty_id INT NOT NULL,
     nameFaculty VARCHAR(255) NOT NULL,
-    departments_id INT NOT NULL,
-    PRIMARY KEY (faculty_id),
-    FOREIGN KEY (departments_id) REFERENCES Departments(departments_id)
 );
 
 CREATE TABLE Departments(
-    departments_id INT NOT NULL AUTO_INCREMENT,
+    departments_id INT NOT NULL ,
     namee VARCHAR(255) NOT NULL,
     locationn VARCHAR(255) NOT NULL,
-    PRIMARY KEY (departments_id)
+    faculty_id INT NOT NULL
 );
 
 CREATE TABLE Teacher(
-    teacher_id INT NOT NULL AUTO_INCREMENT,
-    department_id INT NOT NULL,
+    teacher_id INT NOT NULL,
     course_id INT NOT NULL,
     academicSession_id INT NOT NULL,
     assists_id INT NOT NULL,
     results_id INT NOT NULL,
-    timeConexion_id INT NOT NULL,
-    PRIMARY KEY (teacher_id),
-    FOREIGN KEY (department_id) REFERENCES Departments(departments_id),
-    FOREIGN KEY (course_id) REFERENCES Courses(course_id),
-    FOREIGN KEY (academicSession_id) REFERENCES AcademicSession(academicSession_id),
-    FOREIGN KEY (assists_id) REFERENCES Assists(assists_id),
-    FOREIGN KEY (results_id) REFERENCES Results(results_id),
-    FOREIGN KEY (timeConexion_id) REFERENCES TimeConexion(timeConexion_id)
+    timeConexion_id INT NOT NULL
 );
 
 CREATE TABLE InformationBasic(
     namee VARCHAR(255) NOT NULL,
     lastName VARCHAR(255) NOT NULL,
     birthDate DATE NOT NULL,
-    document INT NOT NULL AUTO_INCREMENT,
+    document INT NOT NULL,
     phoneNum INT NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    PRIMARY KEY (document)
+    email VARCHAR(255) NOT NULL
+
 );
 
 CREATE TABLE Results(
-    results_id INT NOT NULL AUTO_INCREMENT,
-    exam_id INT NOT NULL,
-    PRIMARY KEY (results_id),
-    FOREIGN KEY (exam_id) REFERENCES Exam(exam_id)
+    results_id INT NOT NULL,
+    exam_id INT NOT NULL
 );
 
 CREATE TABLE Exam(
-    exam_id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (exam_id)
+    exam_id INT NOT NULL
 );
 
 CREATE TABLE Assists(
-    assists_id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (assists_id)
+    assists_id INT NOT NULL
 );
 
 CREATE TABLE InformationBasicTeacher(
-    informationTeacher_id INT NOT NULL AUTO_INCREMENT,
+    informationTeacher_id INT NOT NULL,
     namee VARCHAR(255) NOT NULL,
     lastName VARCHAR(255) NOT NULL,
     phoneNum INT NOT NULL,
-    department_id INT NOT NULL,
-    PRIMARY KEY (informationTeacher_id),
-    FOREIGN KEY (department_id) REFERENCES Departments(departments_id)
+    department_id INT NOT NULL
 );
 
 CREATE TABLE Alumns(
-    alumn_id INT NOT NULL AUTO_INCREMENT,
+    alumn_id INT NOT NULL ,
     informationBasic_id INT NOT NULL,
-    academicSessions_id INT NOT NULL,
     courses_id INT NOT NULL,
-    faculty_id INT NOT NULL,
-    exam_id INT NOT NULL,
-    PRIMARY KEY (alumn_id),
-    FOREIGN KEY (informationBasic_id) REFERENCES InformationBasic(document),
-    FOREIGN KEY (academicSessions_id) REFERENCES AcademicSession(academicSession_id),
-    FOREIGN KEY (courses_id) REFERENCES Courses(course_id),
-    FOREIGN KEY (faculty_id) REFERENCES Faculty(faculty_id),
-    FOREIGN KEY (exam_id) REFERENCES Exam(exam_id)
+    department_id INT NOT NULL,
+    exam_id INT NOT NULL
 );
 
 CREATE TABLE Courses(
-    course_id INT NOT NULL AUTO_INCREMENT,
-    academicSession_id INT NOT NULL,
-    PRIMARY KEY (course_id),
-    FOREIGN KEY (academicSession_id) REFERENCES AcademicSession(academicSession_id)
+    course_id INT NOT NULL ,
+    academicSession_id INT NOT NULL
 );
+
+CREATE TABLE TimeConexion(
+    timeConexion_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+);
+
+ALTER TABLE Inscription ADD CONSTRAINT pk_Inscription_id PRIMARY KEY(inscription_id);
+
+ALTER TABLE Register ADD CONSTRAINT pk_register_id PRIMARY KEY(register_id);
+ALTER TABLE AcademicSession ADD CONSTRAINT pk_academicSession_id PRIMARY KEY(academicSession_id);
+ALTER TABLE Faculty ADD CONSTRAINT pk_faculty_id PRIMARY KEY(faculty_id);
+
+
+ALTER TABLE Departments ADD CONSTRAINT pk_departments PRIMARY KEY(departments_id);
+ALTER TABLE Departments ADD CONSTRAINT fk_Faculty_Departments FOREIGN KEY(faculty_id) REFERENCES Faculty(faculty_id);
+
+
+
+ALTER TABLE Teacher ADD CONSTRAINT pk_teacher_id PRIMARY KEY(teacher_id);
+ALTER TABLE Teacher ADD CONSTRAINT fk_Departments_Teacher FOREIGN KEY (teacher_id)REFERENCES Departments(departments_id);
+ALTER TABLE Teacher ADD CONSTRAINT pk_TimeConexion_Teacher FOREIGN KEY (teacher_id)REFERENCES TimeConexion(timeConexion_id);
+ALTER TABLE Teacher ADD CONSTRAINT pk_AcademicSession_Teacher FOREIGN KEY(teacher_id) REFERENCES AcademicSession (academicSession_id);
+
+
+
+ALTER TABLE InformationBasic ADD CONSTRAINT pk_document_id PRIMARY KEY(document);
+ALTER TABLE Results ADD CONSTRAINT pk_results_id PRIMARY KEY(results_id);
+ALTER TABLE Exam ADD CONSTRAINT pk_exam_id PRIMARY KEY(exam_id);
+ALTER TABLE Assists ADD CONSTRAINT pk_assists_id PRIMARY KEY(assists_id);
+ALTER TABLE InformationBasicTeacher ADD CONSTRAINT pk_informationBasicTeacher_id PRIMARY KEY(informationBasicTeacher_id);
+
+ALTER TABLE Alumns ADD CONSTRAINT pk_alumn_id PRIMARY KEY(alumn_id);
+ALTER TABLE Alumns DROP COLUMN academicSessions_id;
+ALTER TABLE Alumns DROP COLUMN faculty_id;
+ALTER TABLE Alumns ADD COLUMN department_id INT NOT NULL;
+
+
+ALTER TABLE Courses ADD CONSTRAINT pk_courses_id PRIMARY KEY(courses_id);
+
+
+ALTER TABLE TimeConexion ADD CONSTRAINT pk_timeConexion_id PRIMARY KEY(timeConexion_id);
+
+
+
+
