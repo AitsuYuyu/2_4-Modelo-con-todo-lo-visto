988dfb153d9e1418f076c450f6b69b07ca109b73
AitsuYuyu <136203413+AitsuYuyu@users.noreply.github.com>
2023-11-02 07:45:29 -0500

    feat: :sparkles: remplaze the entities

--- 2_4EstructureSimple/drawSQL-mysql-export-2023-11-02.sql
+++ 2_4EstructureSimple/drawSQL-mysql-export-2023-11-02.sql
@@ -1,30 +1,29 @@
 CREATE TABLE Inscription(
-    inscription_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    document INT NOT NULL,
+    inscription_id INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    document INT UNIQUE NOT NULL,
     dateInscription DATE NOT NULL
 );
 CREATE TABLE Register(
-    register_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    informationBasic_id INT NOT NULL,
+    register_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    informationBasic_id INT UNIQUE NOT NULL,
     faculty_id BIGINT NOT NULL,
     course_id INT NOT NULL
 );
-CREATE TABLE Academic Session(
-    academicSession_id INT  NOT NULL AUTO_INCREMENT PRIMARY KEY
+CREATE TABLE AcademicSession(
+    academicSession_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 CREATE TABLE Faculty(
-    faculty_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nameFaculty VARCHAR(255) NOT NULL,
-    departments_id INT NOT NULL
+    faculty_id INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nameFaculty VARCHAR(255) NOT NULL
 );
 CREATE TABLE Departments(
-    departments_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    namee VARCHAR(255) NOT NULL,
-    locationn VARCHAR(255) NOT NULL
+    departments_id INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    faculty_id INT NOT NULL
 );
 CREATE TABLE Teacher(
-    teacher_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_id INT NOT NULL,
+    teacher_id INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
     course_id INT NOT NULL,
     academicSession_id INT NOT NULL,
     assists_id INT NOT NULL,
@@ -32,37 +31,37 @@ CREATE TABLE Teacher(
     timeConexion_id INT NOT NULL
 );
 CREATE TABLE InformationBasic(
-    namee VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     lastName VARCHAR(255) NOT NULL,
     birthDate DATE NOT NULL,
-    document INT NOT NULL,
+    document INT NOT NULL PRIMARY KEY,
     phoneNum INT NOT NULL,
-    email VARCHAR(255) NOT NULL
+    email VARCHAR(255) UNIQUE NOT NULL
 );
-
+ALTER TABLE
+    Information Basic ADD PRIMARY KEY(document);
 CREATE TABLE Results(
-    results_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    results_id INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
     exam_id INT NOT NULL
 );
 CREATE TABLE Exam(
     exam_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 CREATE TABLE Assists(
-    assists_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+    assists_id INT  NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 CREATE TABLE InformationBasicTeacher(
-    informationTeacher_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    namee VARCHAR(255) NOT NULL,
+    informationTeacher_id INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
     lastName VARCHAR(255) NOT NULL,
-    phoneNum INT NOT NULL,
+    phoneNum INT UNIQUE NOT NULL,
     department_id INT NOT NULL
 );
 CREATE TABLE Alumns(
     alumn_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     informationBasic_id INT NOT NULL,
-    academicSessions_id INT NOT NULL,
     courses_id INT NOT NULL,
-    faculty_id INT NOT NULL,
+    department_id INT NOT NULL,
     exam_id INT NOT NULL
 );
 CREATE TABLE Courses(
@@ -72,6 +71,3 @@ CREATE TABLE Courses(
 CREATE TABLE TimeConexion(
     timeConexion_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
-
-ALTER TABLE InformationBasic ADD PRIMARY KEY(document);
-
